b1fb85694712c790db66538982ff9adc5cf19d69
dasole123 <dasole1234@gmail.com>
2022-06-23 10:58:33 +0900

    키워드 - 자잘한 내용 추가

--- facebook/src/main/java/com/clone/facebook/controller/PostController.java
+++ facebook/src/main/java/com/clone/facebook/controller/PostController.java
@@ -2,12 +2,10 @@ package com.clone.facebook.controller;
 
 import com.clone.facebook.dto.PostRequestDto;
 import com.clone.facebook.dto.PostResponseDto;
-import com.clone.facebook.models.TokenDecode;
 import com.clone.facebook.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -21,12 +19,12 @@ public class PostController {
         postService.postPost(postRequestDto, authorization);
     }
 
-    @GetMapping("/api/board")
+    @GetMapping("/board")
     public List<PostResponseDto> getPosts(){
         return postService.getPosts();
     }
 
-    @GetMapping("/api/board/user/{postId}")
+    @GetMapping("/board/user/{postId}")
     public PostResponseDto getPostDetail(@PathVariable Long postId){
         return postService.getPostDetail(postId);
     }
